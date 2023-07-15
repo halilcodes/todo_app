388c47035e2017073b768ff81339dabd5007cf63
Halil_Hasmer <halilpython@gmail.com>
2023-07-16 00:12:28 +0300

    finish day 6

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ todos = []
 
 def show_todos(todo_list):
     for num, do in enumerate(todo_list):
-        print(f"{num}- {do.title()}")
+        print(f"{num}- {do.title()}", end="")
+    print()
 
+
+with open("todo_list.txt", "r", encoding="utf8") as file:
+    for line in file:
+        todos.append(line)
 
 while True:
     user_action = input("Type add, show, edit or exit: ").casefold().strip()
-
     match user_action:
         case 'add':
-            todo = input('Enter a todo: ')
+            todo = input('Enter a todo: ') + "\n"
             todos.append(todo)
+            file = open("todo_list.txt", "w")
+            file.writelines(todo)
+            file.close()
         case 'show' | 'display':
             show_todos(todos)
         case 'edit':
@@ -26,4 +33,7 @@ while True:
         case _:
             print('Unknown command!')
 
+with open("todo_list.txt", "w", encoding="utf8") as file:
+    for todo in todos:
+        file.write(todo)
 print('Bye!')
