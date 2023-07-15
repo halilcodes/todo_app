29c0f81ea10b9363e579ad0cedabf741972b0bb7
Halil_Hasmer <halilpython@gmail.com>
2023-07-15 13:37:39 +0300

    2 days prograss

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 todos = []
 
+
+def show_todos(todo_list):
+    for num, do in enumerate(todo_list):
+        print(f"{num}- {do.title()}")
+
+
 while True:
-    user_action = input("Type add, show or exit: ").casefold().strip()
+    user_action = input("Type add, show, edit or exit: ").casefold().strip()
 
     match user_action:
         case 'add':
             todo = input('Enter a todo: ')
             todos.append(todo)
         case 'show' | 'display':
-            for todo in todos:
-                print(todo.title())
+            show_todos(todos)
+        case 'edit':
+            show_todos(todos)
+            index = int(input("Select list item to edit: "))
+            new_input = input("Enter new list item: ")
+            print(f"{todos[index]} changed with {new_input}")
+            todos[index] = new_input
         case 'exit':
             break
         case _:
