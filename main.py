14aa632d12dff6333202223a624c43d59e24e164
Halil_Hasmer <halilpython@gmail.com>
2023-07-16 17:39:46 +0300

    progress to day 11

--- main.py
+++ main.py
@@ -1,39 +1,53 @@
-todos = []
-
-
-def show_todos(todo_list):
+def show_list(todo_list):
+    print(todo_list)
     for num, do in enumerate(todo_list):
         print(f"{num}- {do.title()}", end="")
     print()
 
 
-with open("todo_list.txt", "r", encoding="utf8") as file:
-    for line in file:
-        todos.append(line)
+try:
+    with open("todo_list.txt", "r+", encoding="utf8") as file:
+        todos = file.readlines()
+except FileNotFoundError:
+    with open("todo_list.txt", "w", encoding="utf8") as file:
+        todos = []
 
 while True:
-    user_action = input("Type add, show, edit or exit: ").casefold().strip()
-    match user_action:
-        case 'add':
+    user_action = input("Type add, show, edit, complete or exit: ").casefold().strip()
+    if user_action.startswith('add'):
+        if user_action > 'add':
+            todo = user_action[4:].strip() + "\n"
+            todos.append(todo)
+        else:
             todo = input('Enter a todo: ') + "\n"
             todos.append(todo)
-            file = open("todo_list.txt", "w")
-            file.writelines(todo)
-            file.close()
-        case 'show' | 'display':
-            show_todos(todos)
-        case 'edit':
-            show_todos(todos)
+        # with open("todo_list.txt", "a", encoding="utf8") as file:
+        #     file.writelines(todo)
+    elif user_action.startswith('show'):
+        show_list(todos)
+    elif user_action.startswith('edit'):
+        show_list(todos)
+        try:
+            index = int(user_action[5:])
+        except ValueError:
             index = int(input("Select list item to edit: "))
-            new_input = input("Enter new list item: ")
-            print(f"{todos[index]} changed with {new_input}")
-            todos[index] = new_input
-        case 'exit':
-            break
-        case _:
-            print('Unknown command!')
+        new_input = input("Enter new list item: ") + "\n"
+        print(f"{todos[index]} changed with {new_input}")
+        todos[index] = new_input
+
+    elif user_action.startswith('complete'):
+        show_list(todos)
+        try:
+            index = int(input("Select list item that you completed: "))
+            todos.pop(index)
+        except (ValueError, IndexError):
+            print("Invalid input.")
+            continue
+    elif user_action == "exit":
+        break
+    else:
+        print('Unknown command!')
 
 with open("todo_list.txt", "w", encoding="utf8") as file:
-    for todo in todos:
-        file.write(todo)
+    file.writelines(todos)
 print('Bye!')
